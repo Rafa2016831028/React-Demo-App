9ade151dee31175c8606375a1db95e0d96c9e6ff
Rafa2016831028 <jarintasnimaugust20@gmail.com>
2021-03-18 18:07:54 +0600

    Components and props.

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,9 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 
+const user = {
+  firstName:  "Jarin" ,
+  lastName: "Tasnim",
+  details : {
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnrB4dLxZLv6IDtLYZYDsNLWeYlJjeI_g6Sg&usqp=CAU",
+    age : 22,
+    greet: "Hey dude !",
+    salary: 45000,
+    designation: "Junior Software Engineer"
+  }
+}
+
+function UserComponent(props) {
+  return(
+    <div>
+      <h1>{props.firstName}+" "+{props.lastName}</h1>
+      <img src={props.details.url}></img>
+    </div>
+  );
+}
 function App() {
   return (
-    <div><h1>Hey !</h1></div>
+    <UserComponent 
+    firstName={user.firstName}
+    lastName={user.lastName}
+    details = {user.details}
+    ></UserComponent>
   );
 }
 
--- demo/src/index.js
+++ demo/src/index.js
@@ -4,9 +4,35 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+const user = {
+  firstName:  "Jarin" ,
+  lastName: "Tasnim",
+  details : {
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnrB4dLxZLv6IDtLYZYDsNLWeYlJjeI_g6Sg&usqp=CAU",
+    age : 22,
+    greet: "Hey dude !",
+    salary: 45000,
+    designation: "Junior Software Engineer"
+  }
+}
+
+function UserComponent(props) {
+  return(
+    <div>
+      <h1>{props.firstName}+" "+{props.lastName}</h1>
+      <img src={props.details.url}></img>
+      <div>{props.details.designation}</div>
+    </div>
+  );
+}
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <UserComponent 
+    firstName={user.firstName}
+    lastName={user.lastName}
+    details = {user.details}
+    ></UserComponent>
   </React.StrictMode>,
   document.getElementById('root')
 );
