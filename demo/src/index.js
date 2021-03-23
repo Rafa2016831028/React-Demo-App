3e08547f026a20e4bddb2f40ce6606125871beb1
Rafa2016831028 <jarintasnimaugust20@gmail.com>
2021-03-23 13:13:20 +0600

    added firebase to the project.

--- demo/src/index.js
+++ demo/src/index.js
@@ -8,12 +8,28 @@ import Practice from './main/props_practice';
 import Clock from './main/Component'
 import EventHandle from './main/EventHandle'
 import Page from './main/PreventPageFromRendering';
+import EmployeeForm from './main/form';
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDpQSjuW6jHNMslzK3zlVVVoCkEojONTUc",
+  authDomain: "react-demo-app-944b9.firebaseapp.com",
+  databaseURL: "https://react-demo-app-944b9-default-rtdb.firebaseio.com",
+  projectId: "react-demo-app-944b9",
+  storageBucket: "react-demo-app-944b9.appspot.com",
+  messagingSenderId: "670213470061",
+  appId: "1:670213470061:web:3ab8044c3f9f6dbe488c64",
+  measurementId: "G-EMK2MJPPW9"
+};
+
+firebase.initializeApp(firebaseConfig);
 ReactDOM.render(
   <React.StrictMode>
     <div>
-    <Clock/>
+    {/* <Clock/>
     <EventHandle greet="Hey dude!" designation="JSE"/>
-    <Page/>
+    <Page/> */}
+    <EmployeeForm/>
     </div>
   </React.StrictMode>,
   document.getElementById('root')
---
+++ demo/src/main/form.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+
+class EmployeeForm extends React.Component{
+    constructor(props){
+        super(props);
+        this.state =({id:'',
+        name:"",
+        designation:""
+    })
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event){
+        //debugger
+        this.setState({value: event.target.value})
+    }
+
+    handleSubmit(event){
+        alert(this.state.value);
+        event.preventDefault();
+    }
+
+    render(){
+        return <div>
+            <form onSubmit={this.handleSubmit}>
+               <div>
+               <label>ID : 
+                    <input type="text" value={this.state.value} onChange={this.handleChange}></input>
+                </label>
+                
+                <label>Name : 
+                    <input type="text" value={this.state.value} onChange={this.handleChange}></input>
+                </label>
+               </div>
+               <input value="Submit" type="submit" ></input>
+            </form>
+        </div>
+    }
+
+}
+
+export default EmployeeForm;
